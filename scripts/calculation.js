fa5adc934541d9db5b2a8bf3319d960876823b11
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-02-18 20:02:19 +0600

    all quick functions and validations done

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -5,28 +5,63 @@ function stringToFloat(id){
     let bfloat = parseFloat(bstring);
     return bfloat;
 }
-//converts float to string
+// result adder function
+function addResult(type,area){
+    newList = document.createElement('li');
+    newList.innerHTML = `<div class="flex flex-row justify-between w-full mt-6">
+    <h1 id="result-type" class="text-base mr-1">${type}</h1>
+    <h1 class="mx-1"><span>${area}</span>cm<sup>2</sup></h1>
+    <button class="ml-1 bg-blue-500 text-xs text-white py-1 px-2 rounded">Convert to m<sup>2</sup></button>
+    </div>
+    `
+    document.getElementById('result-window').appendChild(newList);
+}
 
+// for all calculations
+function quickResult(field1id,field2id,btnid,typee){
+    document.getElementById(btnid).addEventListener('click',function(){
+        let w = stringToFloat(field1id);
+        let l = stringToFloat(field2id);
+        if(typee == 'Triangle' || typee == 'Rhombus'){
+            let area = (0.5*w*l).toFixed(2);
+            if(isNaN(area) != true){
+                addResult(typee,area);
+            }
+            else{
+                alert("Please enter valid number in both fields.")
+            }
+        }
+        else if (typee == 'Rectangle' || typee == 'Parallelogram' || typee == "Pentagon"){
+            let area = (w*l).toFixed(2);
+            if(isNaN(area) != true){
+                addResult(typee,area);
+            }
+            else{
+                alert("Please enter valid number in both fields.")
+            }
+        }
+        else if (typee == 'Ellipse'){
+            let area = (3.1416*w*l).toFixed(2);
+            if(isNaN(area) != true){
+                addResult(typee,area);
+            }
+            else{
+                alert("Please enter valid number in both fields.")
+            }
+        }
+        
+    })
+}
 
-
-document.getElementById('triangle-btn').addEventListener('click',function(){
-
-    b = stringToFloat('triangle-b');
-    h = stringToFloat('triangle-h');
-    area = (0.5*b*h).toFixed(2);
-    if(isNaN(area) != true){
-        console.log(area);
-        newList = document.createElement('li');
-        newList.innerHTML = `<div class="flex flex-row justify-between w-full mt-6">
-        <h1 id="result-type" class="text-base mr-1">Triangle</h1>
-        <h1 class="mx-1"><span>${area}</span>cm<sup>2</sup></h1>
-        <button class="ml-1 bg-blue-500 text-xs text-white py-1 px-2 rounded">Convert to m<sup>2</sup></button>
-        </div>
-        `
-        console.log(newList);
-        document.getElementById('result-window').appendChild(newList);
-    }
-    else{
-        alert("Please enter valid number in both fields.")
-    }
-})
+// triangle
+quickResult('triangle-b','triangle-h','triangle-btn','Triangle');
+// rectangle
+quickResult('rectangle-w','rectangle-l','rectangle-btn','Rectangle');
+// Parallelogram
+quickResult('parallel-b','parallel-h','parallel-btn','Parallelogram');
+// pentagon
+quickResult('penta-p','penta-b','penta-btn','Pentagon');
+// rhombus
+quickResult('rhombus-d1','rhombus-d2','rhombus-btn','Rhombus');
+// ellipse
+quickResult('ellipse-a','ellipse-b','ellipse-btn','Ellipse');
